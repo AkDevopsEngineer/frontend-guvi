4395f167a064595a82b0c85f548110608c66a223
AkDevopsEngineer <ajaxajith@protonmail.com>
2023-01-22 23:51:18 +0530

    deploy file added

--- build.sh
+++ build.sh
@@ -4,12 +4,6 @@ IMAGE_NAME="ajithaws02/frontend-guvi-dev"
 DATE=`date +%Y.%m.%d.%H.%M`
 IMAGE_TAG=$DATE 
 
-echo "Stop all running containers"
-docker stop $(docker ps -aq)
-
-echo "Remove all containers"
-docker rm $(docker ps -aq)
-
 echo "Remove Exisiting none tag images"
 docker rmi $(docker images --filter "dangling=true" -q --no-trunc)
 
@@ -20,12 +14,10 @@ echo "Building Docker image ${IMAGE_NAME}:${IMAGE_TAG}, and tagging as latest"
 
 docker build -t "${IMAGE_NAME}:${IMAGE_TAG}" .
 
-result=$(docker images -q ${IMAGE_NAME}:${IMAGE_TAG})
-if [[ -n "$result" ]]; then
-  echo 'Build image Present.'
-else
-  echo 'No such Build image!'
-fi
+echo 'Build image Present.'
+docker images -q ${IMAGE_NAME}:${IMAGE_TAG}
 
 echo "Authenticating and pushing image to Docker Hub"
+docker login -u "ajithaws02" -p "Welcomeajith@1997" docker.io
 docker push "${IMAGE_NAME}:${IMAGE_TAG}"
+sleep 20
